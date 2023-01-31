74e5627646c01fd54133381130c6c6ee33377459
Jeroen Dalsem <jdalsem@coldtrick.com>
2023-01-31 15:27:14 +0100

    added settings for editor toolbars

--- mod/ckeditor/views/default/ckeditor/config/simple.js
+++ mod/ckeditor/views/default/ckeditor/config/simple.js
@@ -1,7 +1,7 @@
-define(['jquery', 'ckeditor/config/base', 'ckeditor/config/mentions', 'ckeditor/config/file_upload'], function($, base, mentions, file_upload) {
+define(['jquery', 'elgg', 'ckeditor/config/base', 'ckeditor/config/mentions', 'ckeditor/config/file_upload'], function($, elgg, base, mentions, file_upload) {
 	return $.extend(base, mentions, file_upload, {
 		toolbar: {
-			items: [
+			items: elgg.data.ckeditor.toolbar_simple || [
 				'Bold', 'Italic', 'Underline', 'Strikethrough', 'RemoveFormat'
 			]
 		}
