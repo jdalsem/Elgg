c1aca3439f379a38ae5ee87f1007a9d8907797e8
Jeroen Dalsem <jdalsem@coldtrick.com>
2023-01-31 12:53:12 +0100

    checked all old features

--- mod/ckeditor/views/default/ckeditor/config/base.js
+++ mod/ckeditor/views/default/ckeditor/config/base.js
@@ -8,6 +8,10 @@ define(['elgg'], function(elgg) {
 			viewportOffset: {
 				top: topbar_height
 			}
-		}
+		},
+		removePlugins: [
+			'MediaEmbedToolbar', // is not yet supported in CKEditor5 but throws console warning
+			'MediaEmbed' // this plugin only provides backend oembed support. Can be enabled by plugin that provides frontend support
+		]
 	};
 });
